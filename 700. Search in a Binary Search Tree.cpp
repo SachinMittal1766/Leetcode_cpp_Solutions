61c0fd8e2281546ba98a334a8252e827c3c60958
Sachin Mittal <sachinmittal24687@gmail.com>
2021-10-05 16:18:13 +0530

    Problem 700

---
+++ 700. Search in a Binary Search Tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root != NULL){
+            if(root->val == val)
+                return root;
+            else if(val>root->val)
+                root = root->right;
+            else
+                root = root->left;
+        }
+        return NULL;
+    }
+};
